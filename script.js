3fb25d0d0aa85de1fc435c3cdaa2cab383674e44
Henry Lima <157661193+henrylima1@users.noreply.github.com>
2025-06-15 14:52:21 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const pagAtual = window.location.pathname.split('/').pop() || 'index.html';
-    
     const navLinks = document.querySelectorAll('.sidebar-nav .nav-link');
-    
+
     navLinks.forEach(link => {
         link.classList.remove('active');
-        
         if (link.getAttribute('href') === pagAtual) {
             link.classList.add('active');
         }
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-
-    const sidebarToggle = document.getElementById('sidebarToggle');
+    const sidebarToggle = document.querySelectorAll('#sidebarToggle');
     const sidebar = document.querySelector('.sidebar');
-    
-    if (sidebarToggle) {
-        sidebarToggle.addEventListener('click', function() {
-            sidebar.classList.toggle('open');
+
+    if (sidebarToggle && sidebar) {
+        // sidebarToggle.addEventListener('click', function () {
+        //     sidebar.classList.toggle('open');
+        // });
+        sidebarToggle.forEach(toggle => {
+            toggle.addEventListener('click', function () {
+                sidebar.classList.toggle('open');
+            });
         });
     }
-}); 
+});
